7c593eb0d3e26d8f1524b12d8a44c5d50fa1cae5
Wil Wardrop <wwardrop@gmail.com>
2022-05-25 14:33:24 -0500

    exercise 6

--- sales_agent_invoices.sql
+++ sales_agent_invoices.sql
@@ -2,5 +2,5 @@ SELECT e.FirstName || " " || e.LastName as FullName, InvoiceId
 FROM Employee e
 JOIN Customer c
 ON e.EmployeeId = c.SupportRepId
-JOIN Invoice i
+JOIN Invoice i 
 ON c.CustomerId = i.CustomerId
