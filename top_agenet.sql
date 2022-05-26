84ee94d9301d1b6c80cb916b4c4e8cec5f442c1b
Wil Wardrop <wwardrop@gmail.com>
2022-05-26 14:23:20 -0500

    exercise 20

---
+++ top_agenet.sql
@@ -0,0 +1,9 @@
+
+SELECT EmployeeName, MAX(Sales)
+FROM (SELECT e.FirstName || " " || e.LastName as EmployeeName, SUM(i.Total) as Sales
+FROM Employee e
+JOIN Customer c
+ON c.SupportRepId = e.EmployeeId
+JOIN Invoice i
+ON i.CustomerId = c.CustomerId
+GROUP BY e.EmployeeId) 
