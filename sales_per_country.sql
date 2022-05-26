1b9e04892a26d722c40b1b0a201a3b778ed91b62
Wil Wardrop <wwardrop@gmail.com>
2022-05-26 14:53:17 -0500

    exercise 23

--- sales_per_country.sql
+++ sales_per_country.sql
@@ -1,4 +1,7 @@
-SELECT i.BillingCountry, SUM(i.total) as CountrySales
+SELECT Country, MAX(CountrySales)
+FROM(SELECT i.BillingCountry as Country, SUM(i.total) as CountrySales
 FROM Invoice i
 GROUP BY i.BillingCountry
 ORDER BY CountrySales DESC
+)
+  
